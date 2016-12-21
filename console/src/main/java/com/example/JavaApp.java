dad17951407f44d08f199f70b4f84d7fe56c77af
dmitry.i.sokolov <DmitryIlichSokolov@gmail.com>
2016-12-21 16:04:47 +0400

    init

--- console/src/main/java/com/example/JavaApp.java
+++ console/src/main/java/com/example/JavaApp.java
@@ -4,6 +4,25 @@ public class JavaApp {
 
     public static void main(String arg[]) {
         System.out.println("Java: Hello world!");
+        Summator summator = new Summator(1, 2);
+        String s = "sum = " + summator.sum();
+        System.out.println(s);
+    }
+
+    static class Summator {
+
+        private final int a;
+        private final int b;
+
+        Summator(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int sum() {
+            return a + b;
+        }
+
     }
 
 }
--- console/src/main/kotlin/me/ilich/javavskotlin/KotlinApp.kt
+++ console/src/main/kotlin/me/ilich/javavskotlin/KotlinApp.kt
@@ -4,6 +4,13 @@ object KotlinApp {
 
     @JvmStatic fun main(arg: Array<String>) {
         println("Kotlin: Hello world!")
+        val summator = Summator(1, 2)
+        val s = "sum = ${summator.sum()}"
+        println(s)
+    }
+
+    class Summator(val a: Int, val b: Int) {
+        fun sum() = a + b
     }
 
 }
